02cd18a4bea9b7a1d56791f8b8e3f347189f1ac2
Patryk <urbanek32@gmail.com>
2016-05-08 15:11:49 +0200

    Emotions - First draft. Displaing texts.

--- Assets/Scripts/EmotionManager.cs
+++ Assets/Scripts/EmotionManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class EmotionManager : MonoBehaviour
 {
@@ -10,19 +11,50 @@ public class EmotionManager : MonoBehaviour
     public bool IsLoadingEmotions = false;
     public float EmotionRegenerationRate;
 
-	void Start ()
+    private float _currentRandomEmotionTimer;
+    [SerializeField] private float _currentRandomEmotionDelay;
+    private bool _isCheckingEmotion = false;
+
+    public Text EmotionTextHeader;
+    public Text EmotionTextSmile;
+    public Text EmotionTextSad;
+
+    private float _currentDisplayTextTimer;
+    private bool _isDisplayingText = false;
+    [SerializeField] private float _displayTextDelay;
+
+    private bool _emotionDetected = false;
+
+    // 1 - Smile | 2 - Sad
+    public int EmotionToFind = 1;
+
+    void Start ()
     {
 	    
 	}
 	
 	void Update ()
-    {
+	{
+	    if (!_isCheckingEmotion)
+	    {
+	        _currentRandomEmotionTimer += Time.deltaTime;
+	    }
+	    if (_isDisplayingText)
+	    {
+	        _currentDisplayTextTimer += Time.deltaTime;
+	        CheckEmotionText();
+	    }
+
         // 1 - gdy emotion meter spadnie ponizej 20% wyswietl komunikat do wywolania emocji
         // gdy gracz zareaguje rozpocznij uzupelnianie amunicji
 	    WatchEmotionMeter();
 
-	    // 2 - losowo wyswietl komunikat o wywolaniu emocji, gdy gracz nie zareguje przyspiesz melancholie
-    }
+        // 2 - losowo wyswietl komunikat o wywolaniu emocji, gdy gracz nie zareguje przyspiesz melancholie
+	    if (!_isCheckingEmotion)
+	    {
+	        CheckRandomEmotion();
+	    }
+	}
 
     private void WatchEmotionMeter()
     {
@@ -43,6 +75,52 @@ public class EmotionManager : MonoBehaviour
             }
 
             PlayerReference.UpdateEmotionMeterSlider(EmotionRegenerationRate * Time.deltaTime);
+        }
+    }
+
+    private void CheckRandomEmotion()
+    {
+        if (_currentRandomEmotionTimer >= _currentRandomEmotionDelay)
+        {
+            _isCheckingEmotion = true;
+
+            _currentRandomEmotionTimer = 0.0f;
+            _currentRandomEmotionDelay = UnityEngine.Random.Range(2, 5);
+
+            EmotionToFind = UnityEngine.Random.Range(1, 3);
+            DisplayEmotionText(true);
+        }
+    }
+
+    private void CheckEmotionText()
+    {
+        if (_currentDisplayTextTimer >= _displayTextDelay)
+        {
+            DisplayEmotionText(false);
+            _isCheckingEmotion = false;
+            _currentDisplayTextTimer = 0.0f;
+
+            if (!_emotionDetected)
+            {
+                Debug.Log("Nie wykryto emocji");
+            }
+        }
+
+        // miejsce na sprawdzenie emocji
+        // ...
+    }
+
+    private void DisplayEmotionText(bool enable)
+    {
+        _isDisplayingText = enable;
+        EmotionTextHeader.gameObject.SetActive(enable);
+        if (EmotionToFind == 1)
+        {
+            EmotionTextSmile.gameObject.SetActive(enable);
+        }
+        else
+        {
+            EmotionTextSad.gameObject.SetActive(enable);
         }
     }
 }
